776047420fad6c77375d5e609e4cee22838c7381
iaraagostinaarias-commits <iarias@byupathway.edu>
2026-05-06 00:03:58 -0300

    Made a profile web page and added the corresponding code to each file (html, css, js)

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// &copy;
+
+const year = document.querySelector("#currentYear");
+const lastModification = document.querySelector("#lastModified");
+const today = new Date();
+
+year.innerHTML = `&copy;<span class="highlight">${today.getFullYear()}</span> &#9827 Iara Agostina Arias &#9827 Argentina`;
+document.getElementById("lastModified").innerHTML = document.lastModified;
