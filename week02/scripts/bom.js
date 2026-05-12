b92fd828d1037c3b25aa597ed3da58cf9f22af15
iaraagostinaarias-commits <iarias@byupathway.edu>
2026-05-11 23:18:42 -0300

    Finished the BoM activity by writing JS code to add/delete items to the list

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,10 +2,24 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteBtn = document.createElement('button');
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteBtn = document.createElement('button');
 
-li.textContent = input.value;
-deleteBtn.textContent = '❌';
-li.append(deleteBtn);
-list.append(li);
+        li.textContent = input.value;
+        deleteBtn.textContent = '❌';
+        li.append(deleteBtn);
+        list.append(li);
+
+        input.focus();
+
+        deleteBtn.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+    }
+
+    input.value = '';
+    input.focus();
+});
