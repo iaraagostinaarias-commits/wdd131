2db949c46752e0858d19368d6e2fee4c72c4b7e2
iaraagostinaarias-commits <iarias@byupathway.edu>
2026-05-19 00:25:49 -0300

    Finished the album of photos page

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,8 +5,8 @@ const today = new Date();
 year.innerHTML = `&copy;<span class="highlight">${today.getFullYear()}</span> &#9827 Iara Agostina Arias &#9827 Argentina`;
 document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
 
-const menuBtn = document.querySelector('#menubtn');
-const navMenu = document.querySelector('.navmenu');
+const menuBtn = document.querySelector('#menuBtn');
+const navMenu = document.querySelector('nav');
 
 menuBtn.addEventListener('click', () => {
     navMenu.classList.toggle('open');
