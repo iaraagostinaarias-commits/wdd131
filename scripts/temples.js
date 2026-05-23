4a7a23539779cf72d881875209808f8689d5a0d2
iaraagostinaarias-commits <iarias@byupathway.edu>
2026-05-22 21:11:34 -0300

    stated the w03 page

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,10 +1,3 @@
-const year = document.querySelector("#currentYear");
-const lastModification = document.querySelector("#lastModified");
-const today = new Date();
-
-year.innerHTML = `&copy;<span class="highlight">${today.getFullYear()}</span> &#9827 Iara Agostina Arias &#9827 Argentina`;
-document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
-
 const menuBtn = document.querySelector('#menuBtn');
 const navMenu = document.querySelector('nav');
 
